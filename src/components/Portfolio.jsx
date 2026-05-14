a8febb4d69279db6435e0c15a404d3a1ddab8a00
Marvin <marvinbernard2018@gmail.com>
2026-05-14 14:27:32 +0300

    Feat: Added portfoilo page

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Portfolio() {
+  return (
+    <section className="portfolio">
+      <h1>Portfolio</h1>
+      <p>Showcase of selected works and creative projects.</p>
+    </section>
+  );
+}
