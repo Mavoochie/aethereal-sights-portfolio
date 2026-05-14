89ba8ba957ba9c70c9221a59ace4f6c2ce423d10
Marvin <marvinbernard2018@gmail.com>
2026-05-14 10:12:34 +0300

    feat: add Navbar component with site navigation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+// src/components/Navbar.jsx
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>ÆTHEREAL SIGHTS</h1>
+      <ul>
+        <li>Home</li>
+        <li>Portfolio</li>
+        <li>Projects</li>
+        <li>Services</li>
+        <li>Booking</li>
+        <li>About</li>
+        <li>The Atelier</li>
+        <li>Journal</li>
+        <li>Cinema</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
