178e973a6af05da6ab90c272785fe4cea07d8f88
Marvin <marvinbernard2018@gmail.com>
2026-05-14 14:36:34 +0300

    Added atelier page

---
+++ src/components/Atelier.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Atelier() {
+  return (
+    <section className="atelier">
+      <h1>The Atelier</h1>
+      <p>A creative space for experimentation, symbolic storytelling, and artistic exploration.</p>
+    </section>
+  );
+}
+g
