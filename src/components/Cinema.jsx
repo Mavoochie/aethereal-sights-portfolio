b32ee889f4974a0afef6111a808a8d2533802952
Marvin <marvinbernard2018@gmail.com>
2026-05-14 14:38:00 +0300

    Addedcinema pages

---
+++ src/components/Cinema.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Cinema() {
+  return (
+    <section className="cinema">
+      <h1>Cinema</h1>
+      <p>Experimental films and visual essays exploring the unseen layers of human experience.</p>
+    </section>
+  );
+}
