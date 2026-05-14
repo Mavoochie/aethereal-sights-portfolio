1cd013d7810f19ba3b6d619024b5cbf76ecd2b86
Marvin <marvinbernard2018@gmail.com>
2026-05-14 14:29:32 +0300

    Feat: Added Services page

---
+++ src/components/Services.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { creativeFocus } from "../data/data";
+
+export default function Services() {
+  return (
+    <section className="services">
+      <h1>{creativeFocus.title}</h1>
+      <h2>{creativeFocus.subtitle}</h2>
+      <ul>
+        {creativeFocus.items.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
